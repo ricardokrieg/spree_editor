a0b6ae8c197e566efe4a4397acc847ba248ca45e
Ricardo Franco <ricardo.krieg@gmail.com>
2014-03-16 22:38:29 -0300

    update editor_configuration.rb

--- app/models/spree/editor_configuration.rb
+++ app/models/spree/editor_configuration.rb
@@ -2,6 +2,6 @@ class Spree::EditorConfiguration < Spree::Preferences::Configuration
 
   preference :enabled, :boolean, :default => true
   preference :current_editor, :string, :default => 'CKEditor'
-  preference :ids, :text, :default => 'product_description page_body page_content_body'
+  preference :ids, :text, :default => 'product_description page_body'
 
 end
